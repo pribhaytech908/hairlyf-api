20fd51156bf8f4d7ee28cbe040c604c6c9501690
pribhaytech908 <sdhirajkumar490@gmaail.com>
2025-04-06 11:50:10 +0530

    cors error solve

--- server.js
+++ server.js
@@ -3,26 +3,33 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import wishlistRoutes from './routes/wishlistRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import cartRoutes from './routes/cartRoutes.js'
+import wishlistRoutes from './routes/wishlistRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
 import swaggerDocs from "./swagger.js";
 import cors from "cors";
+
 dotenv.config();
 connectDB();
+
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: function (origin, callback) {
+    callback(null, origin);
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/cart",cartRoutes)
+app.use("/api/cart", cartRoutes);
+
 swaggerDocs(app);
+
 const PORT = process.env.PORT || 5000;
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

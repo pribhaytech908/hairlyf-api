22d775f24c457c1c04f0ade8ba82d55a4bcfed0b
pribhaytech908 <sdhirajkumar490@gmaail.com>
2025-04-06 11:43:22 +0530

    cors

--- server.js
+++ server.js
@@ -21,4 +21,8 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/cart",cartRoutes)
 swaggerDocs(app);
 const PORT = process.env.PORT || 5000;
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
